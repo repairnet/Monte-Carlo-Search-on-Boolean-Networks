8c2a97353c1c69f6a34a30ef57981d59f1edf4c1
Loïc Paulevé <loic.pauleve@labri.fr>
2026-03-12 15:37:34 +0100

    test sim mutant

--- jl/test_sim_one.jl
+++ jl/test_sim_one.jl
@@ -21,10 +21,34 @@ end
 # warm-up
 fasync_simulations(bn, outputs, 1, 10, x0)
 
-nb_sims = 100000
-maxsteps = 500
+nb_sims = 10000
+maxsteps = 300
 println("Performing $nb_sims simulations of at most $maxsteps from fixed initial configuration")
 @time result = fasync_simulations(bn, outputs, nb_sims, maxsteps, x0)
 
 ratios = outputs_ratios(result, outputs, bn)
 println(ratios)
+
+mutated = zerocfg(bn)
+mutant = zerocfg(bn)
+
+fasync_simulations(bn, mutated, mutant, outputs, 1, 10, x0)
+
+println("Performing $nb_sims simulations with no mutant of at most $maxsteps from fixed initial configuration")
+@time result = fasync_simulations(bn, mutated, mutant, outputs, nb_sims, maxsteps, x0)
+ratios = outputs_ratios(result, outputs, bn)
+println(ratios)
+
+mutated[bn.index["NICD"]] = true
+mutated[bn.index["p53"]] = true
+mutant[bn.index["NICD"]] = true
+mutant[bn.index["p53"]] = false
+println("Performing $nb_sims simulations with mutant $(bn.nodes[mutated]) of at most $maxsteps from fixed initial configuration")
+@time result = fasync_simulations(bn, mutated, mutant, outputs, nb_sims, maxsteps, x0)
+ratios = outputs_ratios(result, outputs, bn)
+println(ratios)
+
+using Profile
+@profile fasync_simulations(bn, mutated, mutant, outputs, nb_sims, maxsteps, x0)
+Profile.print()
+
